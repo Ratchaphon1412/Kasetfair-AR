c3d6581ee71499b114500fc8594d0bb1206376d5
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2023-01-26 23:50:14 +0700

    Fix bug in header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
-<script setup la
-  components: { Navbar },ng="ts">
+<script setup lang="ts">
 import MapZone from '@/components/MapZone.vue';
 import Navbar from '@/components/Navbar.vue';
 </script>
 
 <template>
     <div>
-        <h1>แผนที่</h1>
+		<div class="px-3 py-2">
+			<h1>แผนที่</h1>
+		</div>
         <MapZone/>
         <Navbar />
     </div>
