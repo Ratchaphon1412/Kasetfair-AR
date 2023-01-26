d7905ed7ef85e99950a7920016603214134b7ab2
Punnatud <punnatud.d@ku.th>
2023-01-27 01:36:37 +0700

    create annoucement bar and add text to navbar

--- src/components/NavbarButton.vue
+++ src/components/NavbarButton.vue
@@ -4,10 +4,26 @@
 
 <template>
     <div class="flex justify-center max-w-screen h-fit">
-        <img src="@/assets/icons/ar_icon.svg" alt="" class="p-1">
-        <img src="@/assets/icons/capsule_icon.svg" alt="" class="p-1">
-        <img src="@/assets/icons/icon.home.svg" alt="" class="p-1">
-        <img src="@/assets/icons/time _icon.svg" alt="" class="p-1">
-        <img src="@/assets/icons/more_icon.svg" alt="" class="p-1">
+        <div class="p-1 flex-col">
+        <img src="@/assets/icons/ar_icon.svg" alt="">
+        <h1 class="text-center pt-1">AR</h1>
+        </div>
+        <div class="p-1 flex-col">
+        <img src="@/assets/icons/capsule_icon.svg" alt="">
+        <h1 class="text-center pt-1">วันวานKU</h1>
+        </div>
+        <div class="p-1 flex-col justify-center">
+        <img src="@/assets/icons/icon.home.svg" alt="">
+        <h1 class="text-center pt-1">หน้าหลัก</h1>
+        </div>
+        <div class="p-1 flex-col">
+        <img src="@/assets/icons/time _icon.svg" alt="">
+        <h1 class="text-center pt-1">ตารางเวลา</h1>
+        </div>
+        <div class="p-1 flex-col">
+        <img src="@/assets/icons/more_icon.svg" alt="">
+        <h1 class="text-center pt-1">เพิ่มเติม</h1>
+        </div>
+
     </div>
 </template>
