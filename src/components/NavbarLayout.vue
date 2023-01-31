7b18923d6a1238d4499274dfd01550b85aaa858a
Punnatud <punnatud.d@ku.th>
2023-02-01 00:35:56 +0700

    add notification button in homepage

--- src/components/NavbarLayout.vue
+++ src/components/NavbarLayout.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue';
 import Annoucement from '@/components/Annoucement.vue'
+
+const props = defineProps<{
+    showAnnouce : boolean,
+}>()
 </script>
 
 <template>
     <div>
 		<slot></slot>
         <div class="bottom-0 w-full fixed">
-            <annoucement />
+            <annoucement v-if="showAnnouce"/>
             <navbar />
         </div>
     </div>
