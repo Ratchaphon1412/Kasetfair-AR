342226f393795b39831ba032d9d58916cbafa6d3
kornavit <kornavit.p@ku.th>
2023-02-01 01:00:15 +0700

    add write the google sheet

--- src/views/FeedBack.vue
+++ src/views/FeedBack.vue
@@ -8,115 +8,91 @@
 
 
     <label style="font-weight:bolder" class="pb-1 pl-10">รายงานปัญหา</label><br>
-    <!-- take the photo -->
-    <label class="pl-10">แนบรูปภาพ</label>
+    
+    <label class="pl-10">แนบรูปภาพ </label>
+    <label style="color: red;">*</label>
 
-    <!-- picture 1 -->
-    <div class="grid grid-cols-3 grid-rows-1 gap-4">
-        <label for="dropzone-file"
-            class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-            <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor"
-                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12">
-                    </path>
-                </svg>
-                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to
-                        upload</span> or
-                    drag and drop</p>
-                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-            </div>
-            <input id="dropzone-file" type="file" class="hidden" />
-        </label>
-
-        <!-- picture 2 -->
-        <label for="dropzone-file"
-            class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-            <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor"
-                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12">
-                    </path>
-                </svg>
-
-                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to
-                        upload</span> or
-                    drag and drop</p>
-                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-            </div>
-            <input id="dropzone-file" type="file" class="hidden" />
-        </label>
-
-        <!-- picture 3 -->
-        <label for="dropzone-file"
-            class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-            <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor"
-                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12">
-                    </path>
-                </svg>
-                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to
-                        upload</span> or
-                    drag and drop</p>
-                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-            </div>
-            <input id="dropzone-file" type="file" class="hidden" />
-        </label>
+    <!-- pick picture -->
+    <div class="pl-10 pr-10">
+    <label for="dropzone-file"
+        class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor"
+                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12">
+                </path>
+            </svg>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to
+                    upload</span> or
+                drag and drop</p>
+            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+        </div>
+        <input id="dropzone-file" type="file" class="hidden" />
+    </label>
     </div>
 
+    <!-- score event -->
+    <br><label class="pl-10">ให้คะแนนการจัดงานเกษตรแฟร์ </label>
+    <label style="color: red;">*</label><br>
+    <i class="pl-10" style="color:gray">เพิ่มเติม ปุ่มทางซ้ายเป็น ลด 0.5 และ ทางขวา เพิ่ม 0.5</i>
 
-        <!-- score event -->
-        <br><label class="pl-10">ให้คะแนนการจัดงานเกษตรแฟร์</label><br>
-        <i class="pl-10" style="color:gray">เพิ่มเติม ปุ่มทางซ้ายเป็น ลด 0.5 และ ทางขวา เพิ่ม 0.5</i>
-        
-        <vue3starRatings v-model="scoreEvent" starSize="50" controlSize="30" controlBg="#ffffff" controlColor="#000000" starColor="#57855E"/>
-        
-        <!-- score application -->
-        <br><label class="pl-10">ให้คะแนนแอพพลิเคชั่น</label><br>
-        <i class="pl-10" style="color:gray">เพิ่มเติม ปุ่มทางซ้ายเป็น ลด 0.5 และ ทางขวา เพิ่ม 0.5</i>
-        
-        <vue3starRatings v-model="scoreApp" starSize="50" controlSize="30" controlBg="#ffffff" controlColor="#000000" starColor="#57855E"/>
+    <vue3starRatings v-model="sendtosheet.scoreEvent" starSize="50" controlSize="30" controlBg="#ffffff"
+        controlColor="#000000" starColor="#57855E" />
 
-        <!-- add more detail -->
-        <br><label class="pl-10">ข้อเสนอแนะเพิ่ม</label><br>
-        <div class="text-center pl-10 pr-10">
-            <textarea v-model="moreDetail" rows="4" cols="50" class="w-full rounded"
-                placeholder="ข้อควรปรับปรุง หรือ เพิ่มเนื้อหา...."></textarea>
-        </div>
+    <!-- score application -->
+    <br><label class="pl-10">ให้คะแนนแอพพลิเคชั่น </label>
+    <label style="color: red;">*</label><br>
+    <i class="pl-10" style="color:gray">เพิ่มเติม ปุ่มทางซ้ายเป็น ลด 0.5 และ ทางขวา เพิ่ม 0.5</i>
 
-        <!-- button confirm -->
-        <div class="text-center pl-10 pr-10"><br>
-            <button type="button" @click="confirm"
-                class="text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-full"
-                style="background-color:#57855E">ยืนยัน</button>
-        </div><br>
+    <vue3starRatings v-model="sendtosheet.scoreApp" starSize="50" controlSize="30" controlBg="#ffffff"
+        controlColor="#000000" starColor="#57855E" />
+
+    <!-- add more detail -->
+    <br><label class="pl-10">ข้อเสนอแนะเพิ่ม </label>
+    <label style="color: red;">*</label><br>
+    <div class="text-center pl-10 pr-10">
+        <textarea v-model="sendtosheet.moreDetail" rows="4" cols="50" class="w-full rounded"
+            placeholder="ข้อควรปรับปรุง หรือ เพิ่มเนื้อหา...."></textarea>
+    </div>
+
+    <!-- button confirm -->
+    <div class="text-center pl-10 pr-10"><br>
+        <button type="button" @click="confirm"
+            class="text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-full"
+            style="background-color:#57855E">ยืนยัน</button>
+    </div><br>
 
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import vue3starRatings from "vue3-star-ratings";
+import axios from "axios";
 
 export default {
     data() {
         return {
-            scoreEvent: 0,
-            scoreApp: 0,
-            moreDetail: ''
+            sendtosheet: {
+                scoreEvent: 0,
+                scoreApp: 0,
+                moreDetail: ''
+            }
         };
     },
     methods: {
-        confirm(){
-            if (this.moreDetail !== '' && this.scoreEvent !== 0 && this.scoreApp !== 0) {
+        confirm() {
+            if (this.sendtosheet.moreDetail !== '' && this.sendtosheet.scoreEvent !== 0 && this.sendtosheet.scoreApp !== 0) {
+                axios.get('https://script.google.com/macros/s/AKfycbzqgx7l8_bQTLt0YS8dkm_vbVevCGAdV0FmkLGgq0yzYd-5oQld5kfHhsddXP-UelH5/exec', {
+                    params: {
+                        scoreEvent: this.sendtosheet.scoreEvent,
+                        scoreApp: this.sendtosheet.scoreApp,
+                        moreDetail: this.sendtosheet.moreDetail
+                    }
+                })
                 alert("ใส่ข้อมูลครบทวนแล้ว")
-                console.log(this.scoreEvent);
-                console.log(this.scoreApp);
                 window.location.reload()
-            }else{
+            } else {
                 alert("กรุณาใส่ให้ครบ")
             }
         }
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,55 +1,26 @@
 <template>
   <div class="grid grid-rows-1 grid-flow-col gap-4">
     <!-- this is aorrow left -->
-    <div class="row-start-1">
-      <a href="https://www.google.com">
+    <div class="row-start-1 pl-3">
+      <a href="../home">
         <i class="fa-solid fa-chevron-left pt-3 pl-2 text-3xl"></i>
       </a>
     </div>
     <!-- buttom to report -->
-    <div class="row-start-1 row-end-1 text-right">
-      <a href="#">
-        <i class="fa-solid fa-circle-exclamation text-3xl pt-4"></i>
-      </a>
+    <div class="row-start-1 row-end-1 text-right pr-2">
+      <i class="fa-solid fa-circle-exclamation text-3xl pt-4"></i>
     </div>
   </div>
-  <p class="text-right pb-2" style="color:#2A5E5C">รายงาน</p>
+  
+  <p class="text-right pb-2 pr-1" style="color:#2A5E5C">รายงาน</p>
 
-  <div>
     <label style="font-weight:bolder" class="pb-1 pl-10">รายงานปัญหา</label><br>
+    
     <!-- take the photo -->
-    <label class="pl-10">แนบรูปภาพ</label>
-    <div class="grid grid-cols-3 grid-rows-1 gap-2">
-      <label for="dropzone-file"
-        class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-        <div class="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor"
-            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
-          </svg>
-          <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or
-            drag and drop</p>
-          <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-        </div>
-        <input id="dropzone-file" type="file" class="hidden" />
-      </label>
-      <!-- take the photo -->
-      <label for="dropzone-file"
-        class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-        <div class="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor"
-            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
-          </svg>
-          <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or
-            drag and drop</p>
-          <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-        </div>
-        <input id="dropzone-file" type="file" class="hidden" />
-      </label>
-      <!-- take the photo -->
+    <label class="pl-10">แนบรูปภาพ </label>
+    <label style="color: red;">*</label>
+
+    <div class="pl-10 pr-10">
       <label for="dropzone-file"
         class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
         <div class="flex flex-col items-center justify-center pt-5 pb-6">
@@ -67,7 +38,8 @@
     </div>
 
     <!-- dropdown problem -->
-    <br><label class="pl-10">หมวดหมู่ปัญหา</label><br>
+    <br><label class="pl-10">หมวดหมู่ปัญหา </label>
+    <label style="color: red;">*</label><br>
     <div class="text-center pl-10 pr-10">
       <select id="problem" class="w-full rounded" v-model="problem">
         <!-- ปัญหาต่างๆ -->
@@ -76,13 +48,15 @@
     </div>
 
     <!-- location -->
-    <br><label class="pl-10">ตำแหน่ง</label><br>
+    <br><label class="pl-10">ตำแหน่ง </label>
+    <label style="color: red;">*</label><br>
     <div class="text-center pl-10 pr-10">
       <input v-model="location" type="text" class="w-full rounded" placeholder="ตำแหน่งที่คุณ....">
     </div>
 
     <!-- explain the information -->
-    <br><label class="pl-10">เพิ่มเติม</label><br>
+    <br><label class="pl-10">เพิ่มเติม </label>
+    <label style="color: red;">*</label><br>
     <div class="text-center pl-10 pr-10">
       <textarea v-model="information" rows="4" cols="50" class="w-full rounded"
         placeholder="คำอธิบายเกี่ยวกับเเหตุการณ์นั้น....."></textarea>
@@ -93,7 +67,6 @@
         class="text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-full"
         style="background-color:#57855E">ยืนยัน</button>
     </div>
-  </div>
 
 
   <!-- this is aorrow down -->
@@ -106,6 +79,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
@@ -117,7 +92,14 @@ export default {
   },
   methods: {
       confirm(){
-        if (this.problem !== '' && this.location != '' && this.information != '') {
+        if (this.problem !== '' && this.location !== '' && this.information !== '') {
+          axios.get('https://script.google.com/macros/s/AKfycbx2qxF4VMjgROQTEZZfWGAma_tZWrc5-ikuZI7bPV2rQDoe9bEBdABCyvFDq88WOGHsXg/exec',{
+            params:{
+              problem: this.problem,
+              location: this.location,
+              information: this.information
+            }
+          })
           alert("ใส่ครบหมดทุกอย่างแล้ว")
           window.location.reload()
         }else{
