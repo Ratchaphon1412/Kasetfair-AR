094fb57ce66f581c2c299274c331656b2e15c4d7
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2023-01-31 22:33:07 +0700

    Add /timepod to every route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const BASE_URL = '/timepod'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: BASE_URL + '/',
       name: 'welcome',
       component: () => import('../views/Welcome.vue')
     },
     {
-      path: '/home',
+      path: BASE_URL + '/home',
       name: 'home',
       component: () => import('../views/Home.vue')
     },
     {
-      path: '/trois',
+      path: BASE_URL + '/trois',
       name: 'trois',
       component: () => import('../views/Trois.vue')
     }
