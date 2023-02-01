3ec1a67bad07a1cb931057740571510b8529fe7f
Punnatud <punnatud.d@ku.th>
2023-02-01 21:08:21 +0700

    edit ARpage and create ARNavbar

---
+++ src/components/ARNavbar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import NavbarButton from './NavbarButton.vue';
+</script>
+
+<template>
+    <div class="bg-[#AFC2AC] bg-nav ">
+        <div class="flex content-none justify-center max-w-screen h-fit md:mx-16 lg:mx-40 mx-2">
+            <navbar-button 
+                icon="icon.camera.svg" 
+                text="ถ่ายภาพ" 
+                size = "lg"
+                linkPage="home"
+            />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.bg-nav {
+    background-size: 50px 100px !important;
+}
+
+</style>
--- src/views/AR.vue
+++ src/views/AR.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 import NavbarLayout from '@/components/NavbarLayout.vue';
+import ARNavbarLayout from '@/components/ARNavbarLayout.vue';
+import NavbarButton from '@/components/NavbarButton.vue';
 </script>
 
 <template>
-    <navbar-layout>
-		<!-- Code here -->
-    </navbar-layout>
+  <div class ="flex justify-start pl-2 pt-3">
+    <NavbarButton
+    icon ="back_icon.svg"
+    size=""
+    link-page="home"
+    />
+    <div class="pl-3 pt-1">
+      <img src="@/assets/icons/house_icon.svg" alt="icon">
+    </div>
+  </div>
+  <a-r-navbar-layout>
+
+  </a-r-navbar-layout>
 </template>
 
 <style
