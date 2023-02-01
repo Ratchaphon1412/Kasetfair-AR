164a4c56f8239d5e053b65fecdc4c1738ff21eab
PongSuwapat <78334470+PongSuwapat@users.noreply.github.com>
2023-02-02 03:09:20 +0700

    ARNaka 02/02/2022 03.09 Reduce some code

--- src/views/ARNaka.vue
+++ src/views/ARNaka.vue
@@ -65,26 +65,23 @@ export default {
     },
   },
 };
-let marker_visible = { marker1: false, marker2: false , marker3: false, marker4: false};
+         
+      let marker_visible = { marker1: false, marker2: false , marker3: false, marker4: false};
       let leftTime = 0;//totaltime before model that lost all track to be despawn          
       let reset = true;//true = reset everything to default
       let isShowed = false;//check if model show or not
       let spawn = false;//check if model spawn on camera 
       let despawn = false;//check if time for model to despawn
-      let respawn =false;//check if model is too far
-      let switchingDelay = 0;//delay if some marker are lost track and have to change model
+      let isTooFast =false;//check if model is too far
       let currentPosition = new THREE.Vector3(0,0,0);//current model position
       let newPosition = new THREE.Vector3(0,0,0);//new model position that too far from current position 
       let center = new THREE.Vector3(0,0,0);//center between current and new     
-      let invertPortrait = 0;//compete point for invertPortrait
       let landscape = 0 ;//compete point for Landscape
-      let invertLandscape = 0;//compete point for invertLandscape
       let portait = 0;//compete point for portrait
       let checkpoint = 0;//using for rotation to keep trying to using differnce marker track
       let rotationCheckCount = 0;//number of time that check rotation
       let additionX = 0;//defualt addition x for each roation
       let additionY = 0;//defualt addition y for each roation
-      let markerCount = 0;//check if all needed marker are found
       let playAnimation = false;//check if need to run animation for not
       let mixer;//animation-mixer
       let clips;//animation that going to play
@@ -93,8 +90,8 @@ let marker_visible = { marker1: false, marker2: false , marker3: false, marker4:
 //    using when marker is too far away
       let distance = 0.5;//Distance between current point to center point || center to new
       let flippoint = 1;//to check Rotation from  xdiff & y diff
-      let addX = 0;// extra x axis value
-      let addY = 0;// extra y axis value
+      let addX = 2;// extra x axis value
+      let addY = 2;// extra y axis value
       let countdown = 225;//Delay time without tracking before model disappear
  
 // ----------------------------------------------------------------------------------------------------        
@@ -104,12 +101,12 @@ let marker_visible = { marker1: false, marker2: false , marker3: false, marker4:
           let el = this.el;
           el.addEventListener("markerFound", function() {
             marker_visible[el.id] = true;
-            markerCount += 1;
+           
           });
 
           el.addEventListener("markerLost", function() {
             marker_visible[el.id] = false;
-            markerCount -= 1;
+           
           });
           
         }
@@ -154,47 +151,42 @@ let marker_visible = { marker1: false, marker2: false , marker3: false, marker4:
 //        reset all variable value into defualt
           if(reset)
             {
-                console.log("Reset!!!!");
-                this.falseModel.visible =false;          
-                switchingDelay = 0;
+                console.log("Reset!!!!");          
                 checkpoint = 0;
                 portait = 0;
-                invertPortrait = 0;
                 landscape = 0 ;
-                invertLandscape = 0;
                 rotationCheckCount = 0;
-                
+
 
                 isShowed = false;
                 reset = false;
                 spawn = false;
                 despawn = false;  
-                respawn =false;
-                
+                isTooFast =false;
+                playAnimation = false;
                 center.copy(0,0,0);
                 newPosition.copy(0,0,0);
                 currentPosition.copy(0,0,0);
-
-              
+                this.falseModel.rotation.x = THREE.MathUtils.degToRad(0);
+                this.falseModel.rotation.y = THREE.MathUtils.degToRad(0);
+                this.falseModel.rotation.z = THREE.MathUtils.degToRad(0);
             }
 
 
           
 // *************************************************************************************************** 
 //        compute position / rotation / newest material that should be   
-          if( marker_visible["marker2"] && markerCount >= 2)
+          if( marker_visible["marker2"] &&  (marker_visible["marker1"] || marker_visible["marker3"] || marker_visible["marker4"]))
           { 
 //        Search for 2nd tracker to compute rotation sort by tracking efficiency    
               if(marker_visible["marker3"] && checkpoint <= 0)       {this.el3.object3D.getWorldPosition(this.p2); checkpoint = 1;}
               else if(marker_visible["marker1"] && checkpoint <= 1)  {this.el1.object3D.getWorldPosition(this.p2); checkpoint = 2;}
               else if(marker_visible["marker4"] && checkpoint <= 2)    {this.el4.object3D.getWorldPosition(this.p2); checkpoint = 0;}
               else {checkpoint = 0;}
-
               this.el2.object3D.getWorldPosition(this.p1);
 //         Using Diferrence to compute phone rotation
               let xdiff = this.p1.x - this.p2.x;
               let ydiff = this.p1.y - this.p2.y;
-            
               let pseudoXPos = 0;
               let pseudoYPos = 0;
               let pseudoZPos = 0;
@@ -215,7 +207,7 @@ let marker_visible = { marker1: false, marker2: false , marker3: false, marker4:
                    || ((ydiff > flippoint) && (xdiff > flippoint) && checkpoint == 1) 
                    || ((xdiff < - flippoint) && (ydiff > flippoint) && checkpoint == 3) )
                     {
-                      invertPortrait += 1;
+                      portait -= 1;
                       rotationCheckCount += 1;
                     }
 //            Lanscape [home button on left]
@@ -231,95 +223,94 @@ let marker_visible = { marker1: false, marker2: false , marker3: false, marker4:
                    || ((ydiff < -flippoint) && (xdiff > flippoint) && checkpoint == 0) 
                    || ((xdiff > flippoint) && (ydiff > flippoint )&& checkpoint == 1) )
                     {
-                      invertLandscape += 1;
+                      landscape -= 1;
                       rotationCheckCount += 1;
                     }
                 }
             
-//               console.log("---------------------------------------")
-//               console.log("checkpoint  : " + checkpoint);
-//               console.log("xdiff : " + xdiff);
-//               console.log("ydiff : " + ydiff);
-//               console.log("---------------------------------------")
+            //   console.log("---------------------------------------")
+             //  console.log("checkpoint  : " + checkpoint);
+            //   console.log("xdiff : " + xdiff);
+             //  console.log("ydiff : " + ydiff);
+           //    console.log("---------------------------------------")
             
-//               console.log("      Point      ")
-//               console.log("invertPortrait : " + invertPortrait)
-//               console.log("landscape : " + landscape);
-//               console.log("invertLandscape : " + invertLandscape)
-//               console.log("portait : " + portait)
-//               console.log("rotationCheckCount : " + rotationCheckCount +"\n---------------------------------------")
+              console.log("      Point      ")
+               console.log("landscape : " + landscape);
+               console.log("portait : " + portait)
+               console.log("rotationCheckCount : " + rotationCheckCount +"\n---------------------------------------")
             
 //      FInd Winner Rotation       
               if(rotationCheckCount % 5  == 0)
                 {
-                  if ((landscape > invertLandscape ) &&  ( landscape > invertPortrait) &&  ( landscape > portait) )
+                  console.log("-----------------------------------------------\n--------------------------------------------------\n---------------------------------")
+                  if ( landscape > portait  && landscape > 0 && portait < 3 && portait > -3)
                         {
-                          additionX = 5;
-                          additionY = -1;
+                          additionX = 5 + addY;
+                          additionY = -1 + addX;
+                          this.falseModel.rotation.z = THREE.MathUtils.degToRad(270);
                           console.log("Landscape ");
                         }
-                   else if ((invertPortrait > invertLandscape ) &&  ( invertPortrait > landscape ) &&( invertPortrait > portait))
+                   else if (portait < landscape && portait < 0 && landscape < 3 && landscape > -3 )
                         {
-                          additionX = 1;
-                          additionY = -3;
+                          additionX = 1 - addX;
+                          additionY = -3 - addY;
+                          this.falseModel.rotation.z = THREE.MathUtils.degToRad(180);
                           console.log("Invert Portrait ");
                         }
-                    else if((invertLandscape > invertPortrait ) &&  ( invertLandscape > landscape )&& ( invertLandscape > portait))
+                    else if(landscape < portait  && landscape < 0 && portait < 3 && portait > -3)
                         {
-                          additionX = -4;
-                          additionY = 1;
+                          additionX = -4 - addY;
+                          additionY = 1 - addX;
+                          this.falseModel.rotation.z = THREE.MathUtils.degToRad(90);
                           console.log("Invert LandScape ");
                         }
-                    else if(((portait > invertPortrait ) &&  ( portait > landscape ) && ( portait > invertLandscape) ) )
+                    else if( portait > landscape  && portait > 0 && landscape < 3 && landscape > -3)
                         {
-                          additionX = -1;
-                          additionY = 3;
+                          additionX = -1 + addX;
+                          additionY = 3 + addY; 
+                          this.falseModel.rotation.z = THREE.MathUtils.degToRad(0);
                           console.log("Potrait");
                         }
-                    else if(!isShowed)  {reset = true; rotationCheckCount = 0; }
+                    else if(!isShowed)  {despawn = true; }
 
                     checkpoint = 0;
-                    invertPortrait = 0;
                     landscape = 0 ;
-                    invertLandscape = 0;
                     portait = 0;
                 }
-
-                 pseudoXPos = this.p1.x + addX + additionX;
-                 pseudoYPos = this.p1.y + addY + additionY;
+                 pseudoXPos = this.p1.x + additionX;
+                 pseudoYPos = this.p1.y + additionY;
                  pseudoZPos = this.p1.z ;
-                 let vectorOriginPosition = new THREE.Vector3(0,0,0);
-                 vectorOriginPosition.set(pseudoXPos,pseudoYPos,pseudoZPos);
+
 
 // If new position is too far model will slide to it 
-                if((leftTime < countdown - 1)  && respawn == false && leftTime != 0 && ( rotationCheckCount % 5  != 0 || rotationCheckCount   <= 2))
+                if((leftTime < countdown - 1)  && isTooFast == false && leftTime != 0 && ( rotationCheckCount % 5  != 0 || rotationCheckCount   <= 2))
                   { 
-                    if(currentPosition.distanceTo(vectorOriginPosition) > distance * 2  )
-                    {        
-                      newPosition.copy(vectorOriginPosition);
+                    if(currentPosition.distanceTo(new THREE.Vector3(pseudoXPos,pseudoYPos,pseudoZPos)) > distance * 2  )
+                    {   
+                      newPosition.set(pseudoXPos,pseudoYPos,pseudoZPos);
                       let xCen =  (newPosition.getComponent(0) + currentPosition.getComponent(0)) / 2;
                       let yCen =  (newPosition.getComponent(1) + currentPosition.getComponent(1)) / 2;
                       let zCen =  (newPosition.getComponent(2) + currentPosition.getComponent(2)) / 2;
                       center.set(xCen,yCen,zCen);
-                      respawn = true;
+                      isTooFast = true;
                     }
                   }
 // else -> normal tracking
-                  else if(!respawn && rotationCheckCount >= 10)
+                  else if(!isTooFast && rotationCheckCount >= 10 && !despawn)
                     {
                       if(!isShowed)
                           {
-                            this.falseModel.position.z = vectorOriginPosition.getComponent(2);
+                            this.falseModel.position.z = pseudoZPos;
                             spawn = true;
                             isShowed = true;
                           }
                         if(this.falseModel.position.z - pseudoZPos >= 1 ||  this.falseModel.position.z - pseudoZPos <= -1)
                           {
-                             this.falseModel.position.z = (vectorOriginPosition.getComponent(2) + pseudoZPos ) / 2;
+                             this.falseModel.position.z = (pseudoZPos + pseudoZPos ) / 2;
                           }
-                            this.falseModel.position.x = vectorOriginPosition.getComponent(0);
-                            this.falseModel.position.y = vectorOriginPosition.getComponent(1);
-                            currentPosition.copy(vectorOriginPosition);
+                            this.falseModel.position.x = pseudoXPos;
+                            this.falseModel.position.y = pseudoYPos;
+                            currentPosition.set(pseudoXPos,pseudoYPos,pseudoZPos);
                             leftTime = countdown;
                     }
                 }
@@ -330,7 +321,7 @@ let marker_visible = { marker1: false, marker2: false , marker3: false, marker4:
 //  ***************************************************************************************************           
 //        Move when new position is too far
           
-          if(respawn)
+          if(isTooFast)
             {
               let positionToMove = new THREE.Vector3(0,0,0);
               let p1 = new THREE.Vector3(0,0,0);
@@ -342,24 +333,28 @@ let marker_visible = { marker1: false, marker2: false , marker3: false, marker4:
                   if((currentPosition.distanceTo(center) > distance && !center.equals(0,0,0)) || (currentPosition.distanceTo(newPosition) > distance))
                   {
                     if(currentPosition.distanceTo(center) > distance && !center.equals(0,0,0))
-                  { p1.copy(center);
-                    p2.copy(currentPosition);}
+                    { 
+                      p1.copy(center);
+                      p2.copy(currentPosition);
+                    }
 //             move model into newPosition from center     
-                  else if(currentPosition.distanceTo(newPosition) > distance)
-                  { center.copy(0,0,0);
-                    p1.copy(newPosition);
-                    p2.copy(currentPosition);}
+                    else if(currentPosition.distanceTo(newPosition) > distance)
+                    { 
+                      center.copy(0,0,0);
+                      p1.copy(newPosition);
+                      p2.copy(currentPosition);
+                    }
                   
-                    let xCen =  (p1.getComponent(0) + p2.getComponent(0)) / 2;
-                    let yCen =  (p1.getComponent(1) + p2.getComponent(1)) / 2;
-                    let zCen =  (p1.getComponent(2) + p2.getComponent(2)) / 2; 
-                    positionToMove.set(xCen,yCen,zCen);
+                      let xCen =  (p1.getComponent(0) + p2.getComponent(0)) / 2;
+                      let yCen =  (p1.getComponent(1) + p2.getComponent(1)) / 2;
+                      let zCen =  (p1.getComponent(2) + p2.getComponent(2)) / 2; 
+                      positionToMove.set(xCen,yCen,zCen);
                     
-                    this.falseModel.position.x = positionToMove.getComponent(0);
-                    this.falseModel.position.y = positionToMove.getComponent(1);
-                    currentPosition.copy(positionToMove);}
+                      this.falseModel.position.x = positionToMove.getComponent(0);
+                      this.falseModel.position.y = positionToMove.getComponent(1);
+                      currentPosition.copy(positionToMove);}
                   
-                else if(currentPosition.distanceTo(newPosition) < distance) {respawn = false;}
+                  else if(currentPosition.distanceTo(newPosition) < distance) {isTooFast = false;}
                 }
             }       
           
@@ -412,56 +407,56 @@ let marker_visible = { marker1: false, marker2: false , marker3: false, marker4:
           
           
         }});  
+          
+          
+          
+        
 </script>
 
 <style>
 a-scene {
-  height: 100vh;
+  height: 120vh;
 }
-navbar-layout{
+navbar-layout {
   z-index: 50;
+}
+video {
+  margin-left: 0px !important;
 }
 </style>
 
 <template>
-    <a-scene
+       <a-scene
       embedded
       arjs="sourceType: webcam; ; detectionMode: mono_and_matrix; matrixCodeType: 3x3"
+      vr-mode-ui="enabled: false;"
+      loading-screen="enabled: false;"
+      renderer="logarithmicDepthBuffer: true;"
+      id="scene"
+      gesture-detector
     >
-         
-        <a-asset>
-        <a-asset-item
-          id="mdl"
-          src="/models/Cow.gltf"
-                      >
-        </a-asset-item>
-      </a-asset>
          
          
       <a-marker type="barcode" id="marker1" value="14" check-marker>
         <a-box position="0 0.5 0" color="red" opacity="0.8"></a-box>
-        <a-entity id="orgin1"></a-entity>
       </a-marker>
     
       <a-marker type="barcode" id="marker2" value="8" check-marker>
         <a-box position="0 0.5 0" color="red" opacity="0.8"></a-box>
-            <a-entity id="orgin2"></a-entity>
       </a-marker>
     
       <a-marker type="barcode" id="marker3" value="60" check-marker>
         <a-box position="0 0.5 0" color="red" opacity="0.8"></a-box>
-            <a-entity id="orgin3"></a-entity>
       </a-marker>
     
       <a-marker type="barcode" id="marker4" value="58" check-marker>
         <a-box position="0 0.5 0" color="red" opacity="0.8"></a-box>
-        <a-entity id="orgin4"></a-entity>
       </a-marker>
          
                   
 <!--       <a-entity id="vectorOrigin" > </a-entity> -->
 <!--       <a-entity id = "false-model" gesture-handler position = "0 0 0"  gltf-model="#mdl" take-animation></a-entity> -->
-      <a-entity id = "false-model" gesture-handler scale = "2 2 2"  gltf-model="/models/nakraFullBodyColorUV.glb" ></a-entity>
+      <a-entity id = "false-model" gesture-handler visible="false" scale = "2 2 2"  gltf-model="/models/nakraFullBodyColorUV.glb" ></a-entity>
 
       <a-entity id = "camera" camera  ></a-entity>
       <a-entity spawn-model></a-entity>
