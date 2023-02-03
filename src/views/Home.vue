d333cbf08d3a8eb7b7e6c75cb367fb1ee0da5297
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-02-03 19:08:09 +0700

    create action pan svg home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import MapZone from '@/components/MapZone.vue';
-import NavbarButton from '@/components/NavbarButton.vue';
-import NavbarLayout from '@/components/NavbarLayout.vue';
+import MapZone from "@/components/MapZone.vue";
+import NavbarButton from "@/components/NavbarButton.vue";
+import NavbarLayout from "@/components/NavbarLayout.vue";
 </script>
 
 <template>
-    <navbar-layout :show-annouce=true>
-		<div class="px-3 py-2 flex justify-between ">
-			<h1 class="font-bold text-3xl my-auto">แผนที่</h1>
-            <navbar-button
-                icon="notification_icon.svg"
-                size="sm"
-                linkPage="notification" 
-                class="my-auto"
-                />
-		</div>
-        <map-zone/>
-    </navbar-layout>
+  <navbar-layout :show-annouce="true">
+    <div class="px-3 py-2 flex justify-between">
+      <h1 class="font-bold text-3xl my-auto">แผนที่</h1>
+      <navbar-button
+        icon="notification_icon.svg"
+        size="sm"
+        linkPage="notification"
+        class="my-auto"
+      />
+    </div>
+    <map-zone />
+  </navbar-layout>
 </template>
