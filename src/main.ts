657b705c1007e18452e672427df34296054c05da
onrawin <onrawin.h@ku.th>
2023-02-03 14:38:23 +0700

    delete troisjs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { TroisJSVuePlugin } from 'troisjs';
 
 import App from './App.vue'
 import router from './router'
@@ -11,5 +10,4 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(TroisJSVuePlugin)
 app.mount('#app')
