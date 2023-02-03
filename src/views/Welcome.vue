6b32c43340a360ad984a263195560f20793bc10b
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2023-02-04 00:28:39 +0700

    Add new background

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,17 +1,16 @@
-<script setup lang="ts">
-import { onMounted, ref } from 'vue';
-
-onMounted(() => {
-
-});
-</script>
-
 <template>
-    <div>
-        <RouterLink :to="{ name: 'home' }">Home</RouterLink><br/>
-        <img ref="" src="@/assets/images/KasetFairWelcomeBackground.png" class="w-full" alt="background">
+    <div class="bg-welcome">
+        <router-link :to="{ name: 'home' }">
+            <div class="bg-white inline-block px-8 py-2 font-bold text-xl rounded-full hover:bg-slate-200 absolute bottom-12 left-[50%] translate-x-[-50%]">เข้าสู่เว็บไซต์</div>
+        </router-link>
     </div>
 </template>
 
 <style scoped>
+.bg-welcome {
+    background-image: url("@/assets/images/WelcomeBackground.svg");
+    background-size: cover;
+    background-position: center top;
+    height: 100vh;
+}
 </style>
